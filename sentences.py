971899d02a851450fe4d5e55dde857fcc8adbb15
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-09-25 20:35:42 +0100

    pro: sentences.py: abstraction I

--- sentences.py
+++ sentences.py
@@ -4,12 +4,13 @@ product_review = '''This is a fine milk, but the product
   line appears to be limited in available colors. I
   could only find white.'''
 
+def get_matches_for_pattern(pattern_object, items):
+    return [item[0] for item in pattern_object.findall(items)]
+
 sentence_pattern = re.compile(r'(.*?\.)(\s|$)', re.DOTALL)
-matches = sentence_pattern.findall(product_review)
-sentences = [match[0] for match in matches]
+sentences = get_matches_for_pattern(sentence_pattern, product_review)
 
 word_pattern = re.compile(r"([\w\-']+)([\s,.])?")
 for sentence in sentences:
-    matches = word_pattern.findall(sentence)
-    words = [match[0] for match in matches]
+    words = get_matches_for_pattern(word_pattern, sentence)
     print(words)
