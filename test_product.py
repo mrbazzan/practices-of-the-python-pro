ecfb429f54672ae09d093949794babcd1b2c3043
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-11-07 08:59:36 +0100

    integration test for cart operation

--- test_product.py
+++ test_product.py
@@ -11,3 +11,19 @@ class ProductTestCase(unittest.TestCase):
             expected_value,
             small_black_shoes.transform_name_for_sku()
         )
+
+class ShoppingCartTestCase(unittest.TestCase):
+    def test_and_remove_product(self):
+        xl_black_shoes = Product("shoes", "XL", "black")
+        shoe_cart = ShoppingCart()
+
+        self.assertEqual(shoe_cart.products, {})
+
+        shoe_cart.add_product(xl_black_shoes)
+        self.assertEqual(
+            len(shoe_cart.products),
+            1
+        )
+
+        shoe_cart.remove_product(xl_black_shoes)
+        self.assertEqual(shoe_cart.products, {})
